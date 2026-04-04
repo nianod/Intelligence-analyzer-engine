4a861ed7baec6f082eb49e19e34faf09ff5e80be
nianod <arnoldkk422@gmail.com>
2026-04-04 20:28:17 +0300

    fastapi init

--- back/main.py
+++ back/main.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
- 
+from fastapi import FastAPI, HTTPException
+import httpx
 
 app = FastAPI()
+GITHUB_API = "https://api.github.com"
 
 @app.get('/')
 async def landing():
     return{"message": "Hello from FastAPI"}
+
+@app.get("/github/{username}")
+async def get_github_user(username: str):
+    async with httpx.Asyncclient() as client:
+
+
+# print(GITHUB_API)
