3620f2f677d07cb790bc5823e5dd0c1bf9b67595
nianod <arnoldkk422@gmail.com>
2026-05-09 13:50:51 +0300

    fixed cors conflict to match

--- back/main.py
+++ back/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://analyzer-engine.top"],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
